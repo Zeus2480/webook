5641edf93e4f28b89399e5525b377616d960c206
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-03-10 18:21:46 +0530

    fix

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <hello-world />
+
+</div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
