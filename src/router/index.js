7c3e7b72ee9bdf13c25b9b2561d6451a90507424
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-03-11 13:50:23 +0530

    Day 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import RegisterUserDetails from '../views/RegisterUserDetails.vue'
+import RegisterDomain from '../views/RegisterDomain.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect:'/register-userdetails'
+    
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/register-userdetails',
+    name:'RegisterUserDetails',
+    component:RegisterUserDetails
+  },
+  {
+    path:'/register-domain',
+    name:'RegisterDomain',
+    component:RegisterDomain
   }
+  
 ]
 
 const router = new VueRouter({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  prefix: 'tw-',
   theme: {
+    extend: {
+      colors:{
+        'navy-blue':'#10131d',
+        'nav-sec':'#222531',
+        'nav-sec-hover':'#333644'
+
+      },
+      height:{
+        'h-horizontal-img':'133px',
+        '30-vh':'30vh'
+      },
+      width:{
+        'w-horizontal-img':'200px'
+      },
+      borderRadius:{
+        'br-8':'8px'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
